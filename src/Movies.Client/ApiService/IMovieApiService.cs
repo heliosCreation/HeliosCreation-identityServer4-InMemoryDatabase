71ea5ccff296b9b5e1a478f6e8c51cf7a1d2faa6
heliosCreation <reliabledevelopment@outlook.com>
2021-08-30 14:51:55 +0200

    Modified project tree.

---
+++ src/Movies.Client/ApiService/IMovieApiService.cs
@@ -0,0 +1,17 @@
+﻿using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiService
+{
+    public interface IMovieApiService
+    {
+        Task<IEnumerable<Movie>> GetMovies();
+        Task<Movie> GetMovie(Guid id);
+        Task<Movie> CreateMovie(Movie movie);
+        Task<Movie> UpdateMovie(Movie movie);
+        Task DeleteMovie(Guid id);
+    }
+}
---
+++ src/Movies.Client/ApiService/MovieApiService.cs
@@ -0,0 +1,48 @@
+﻿using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiService
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public async Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public Task DeleteMovie(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var movies = new List<Movie>();
+            movies.Add(new Movie
+            {
+                Id = Guid.NewGuid(),
+                Genre = "Drama",
+                Title = "The Shawshank Redemption",
+                Rating = "9.3",
+                ImageUrl = "images/src",
+                ReleaseDate = new DateTime(1994, 5, 5),
+                OwnerId = "alice"
+            });
+            return await Task.FromResult(movies);
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
