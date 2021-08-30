8d37bd41accf14bbcd9ed47deab7552c71abb0e1
heliosCreation <reliabledevelopment@outlook.com>
2021-08-30 14:49:35 +0200

    Init

---
+++ Movies.Client/Startup.cs
@@ -0,0 +1,101 @@
+﻿using IdentityModel;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Movies.Client.ApiService;
+
+namespace Movies.Client
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddScoped<IMovieApiService, MovieApiService>();
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                //Make sure when someone connects, he goes through the OIDC
+                opt.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+
+            })
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, opt =>
+            {
+                opt.AccessDeniedPath = "/Authorization/AccessDenied";
+            })
+            //Use those parameters for OIDC
+            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, opt =>
+            {
+                opt.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                opt.Authority = "https://localhost:5005/";
+                opt.ClientId = "movies_mvc_client";
+                opt.ClientSecret = "secret";
+
+                opt.ResponseType = "code";
+                opt.UsePkce = true;
+
+                //Uncomment these to change the callback path set in the IDP
+                //opt.CallbackPath = new PathString("");
+                opt.Scope.Add("address");
+                opt.Scope.Add("roles");
+                opt.SaveTokens = true;
+                opt.GetClaimsFromUserInfoEndpoint = true;
+
+                //Some claims are filtered by the middleware pipeline. With this command, we remove the filter.
+                //opt.ClaimActions.Remove("nbf");
+                //Some claims might also not be wanted. With this method, we remove them from the claim Identity. Thus, making the cookie lighter.
+                opt.ClaimActions.DeleteClaims("sid", "idp", "s_hash", "auth_time");
+                opt.ClaimActions.MapUniqueJsonKey("role", "role");
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = JwtClaimTypes.GivenName,
+                    RoleClaimType = JwtClaimTypes.Role
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+} 
+
